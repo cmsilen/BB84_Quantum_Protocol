96a5e93f68b32cf132375691a8bb9bd6261d7ba2
carlomazzanti <carlomazzanti00@gmail.com>
2025-12-13 15:46:58 +0100

    code corrections, metrics

--- src/BB84_Protocol.jl
+++ src/BB84_Protocol.jl
@@ -2,25 +2,28 @@ using QuantumSavory
 using GLMakie
 using Random
 
-# paramaters
-num_bits_total = 300            # total exchanged bits
-eavesdropping_event = false     # if there is eavesdropping in the quantum channel
-bit_flip_event = true           # if bit flip events happen
-phase_flip_event = false        # if phase flip events happen
-p = 0.1                         # probability of error in the quantum channel
-k = 0.1                         # fraction of the disclosed key for eavesdropping detection
-seed_gen = 10                   # seed for the random generator
+# ----- PARAMETERS -----
+L_init =                    300     # total exchanged bits
+eavesdropping_event =       false   # if there is eavesdropping in the quantum channel
+bit_flip_event =            false   # if bit flip events happen
+phase_flip_event =          false   # if phase flip events happen
+p =                         0.1     # probability of error in the quantum channel
+k =                         0.1     # fraction of the disclosed key for eavesdropping detection
+bits_mismatch_tolerance =   0.1     # fraction of mismatched key's bits that we are willing to accept before concluding that there ws eavesdropping
+seed_gen =                  10      # seed for the random generator
 
-Random.seed!(seed_gen)
-reg = Register(1) # We only need one qubit at a time
-alice_bases = rand(0:1, num_bits_total) # Alice's random bases. 0 = Z-basis, 1 = X-basis
-alice_bits = rand(0:1, num_bits_total)  # Alice's random bits
-bob_bases = rand(0:1, num_bits_total)   # Bob's random bases
-bob_bits = []
-eves_bases = rand(0:1, num_bits_total) # Eve's random bases
-eves_bits = []
+Random.seed!(seed_gen)              # initializing the random bit generator
+reg = Register(1)                   # represents the quantum channel
+alice_bases = rand(0:1, L_init)     # Alice's random bases. 0 = Z-basis, 1 = X-basis
+alice_bits = rand(0:1, L_init)      # Alice's random bits
+alice_quantum_key = Int[]           # Alice's derived key
+bob_bases = rand(0:1, L_init)       # Bob's random bases
+bob_bits = []                       # Bob's measured bits
+bob_quantum_key = Int[]             # Bob's derived key
+eves_bases = rand(0:1, L_init)      # Eve's random bases
+eves_bits = []                      # Eve's measured bits
 
-for i in 1:num_bits_total
+for i in 1:L_init
     # Alice prepares the qubit
     if alice_bases[i] == 0
         # Z-basis
@@ -79,49 +82,73 @@ for i in 1:num_bits_total
     push!(bob_bits, meas_result)
 end
 
-# seperation of the quantum key by extracting only the matching results
-quantum_key = Int[]
-num_z_corr = 0
-num_x_corr = 0
-for i in 1:num_bits_total
+# seperation of the quantum key by extracting only the bits associated to matching bases
+for i in 1:L_init
     if alice_bases[i] == bob_bases[i]
-        if alice_bases[i] == 1
-            num_z_corr = num_z_corr + 1
-        else
-            num_x_corr = num_x_corr + 1
+        push!(alice_quantum_key, alice_bits[i])
+        push!(bob_quantum_key, bob_bits[i])
+        if alice_bits[i] != bob_bits[i]
+            # print for logging purposes
+            println("different key bit at index ", i)
         end
-
-        push!(quantum_key, alice_bits[i])
     end
 end
-
-println("Sifted key length: ", length(quantum_key))
-println("Sifted key: ", quantum_key)
+println("Sifted key length: ", length(alice_quantum_key))
 
 
 
-# mismatch ratio
-key_length = length(quantum_key)
-global_R_miss = length(quantum_key)/length(alice_bits)
+# ----- METRICS -----
+# global mismatch ratio
+# ratio of the total number of mismatched bits between Alice's and Bob to L_init
+global_R_miss = 0
+mismatched_bits = 0
+for i in 1:L_init
+    if alice_bits[i] != bob_bits[i]
+        mismatched_bits += 1
+    end
+end
+global_R_miss = mismatched_bits / L_init
+println("global mismatch ratio: ", global_R_miss)
 
-num_Z_basis = count(==(0), alice_bases)
-num_X_basis = length(alice_bases)-num_Z_basis
-x_R_miss = num_x_corr/key_length
-z_R_miss = num_z_corr/key_length
+# Z mismatch ratio, X mismatch ratio
+# 1) ratio of mismatched bits to L_init when states are encoded in the Z basis {|0⟩, |1⟩}
+# 2) ratio of mismatched bits to L_init when states are encoded in the X basis {|+⟩, |-⟩}
+
+n_Z_base = 0
+n_Z_base_mismatch = 0
+n_X_base = 0
+n_X_base_mismatch = 0
+for i in 1:L_init
+    if alice_bases[i] == 0
+        n_Z_base += 1
+        if alice_bits[i] != bob_bits[i]
+            n_Z_base_mismatch += 1
+        end
+    end
+    if alice_bases[i] == 1
+        n_X_base += 1
+        if alice_bits[i] != bob_bits[i]
+            n_X_base_mismatch += 1
+        end
+    end
+end
+Z_R_miss = n_Z_base_mismatch / n_Z_base
+X_R_miss = n_X_base_mismatch / n_X_base
+println("Z mismatch ratio: ", Z_R_miss)
+println("X mismatch ratio: ", X_R_miss)
 
 
-
-# eavesdropping detection propability
-p_und(k) = 1-(3/4)^k
-ks = 0:0.1:20               # define the range of k
-fs = p_und.(ks)             # calc function values
+# eavesdropping detection probability
+#p_und(k) = 1-(3/4)^k
+#ks = 0:0.1:20               # define the range of k
+#fs = p_und.(ks)             # calc function values
 
 # printFigure
-fig = Figure(size=(800,400))
-ax = Axis(fig[1, 1], xlabel="k", ylabel="f(k)", title="f(k) = 1 - (3/4)^k")
+#fig = Figure(size=(800,400))
+#ax = Axis(fig[1, 1], xlabel="k", ylabel="f(k)", title="f(k) = 1 - (3/4)^k")
 
 # Plot zeichnen
-lines!(ax, ks, fs, color=:blue, linewidth=2)
+#lines!(ax, ks, fs, color=:blue, linewidth=2)
 
 # Figure anzeigen
-fig
+#fig
