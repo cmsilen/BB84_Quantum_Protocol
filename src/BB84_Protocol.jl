7b28fe7e4e07e3f3be40a4f5741f1516e8b77721
carlomazzanti <carlomazzanti00@gmail.com>
2025-12-13 20:48:34 +0100

    framework for experiments

--- src/BB84_Protocol.jl
+++ src/BB84_Protocol.jl
@@ -1,164 +1,146 @@
 using QuantumSavory
 using GLMakie
 using Random
+using DataFrames
+using CSV
 
-# ----- PARAMETERS -----
-L_init =                    300     # total exchanged bits
-eavesdropping_event =       false   # if there is eavesdropping in the quantum channel
-bit_flip_event =            false   # if bit flip events happen
-phase_flip_event =          false   # if phase flip events happen
-p =                         0.1     # probability of error in the quantum channel
-k =                         0.1     # fraction of the disclosed key for eavesdropping detection
-bits_mismatch_tolerance =   0.1     # fraction of mismatched key's bits that we are willing to accept before concluding that there ws eavesdropping
-seed_gen =                  10      # seed for the random generator
+function simulate_bb84(L_init, eavesdropping_event, bit_flip_event, phase_flip_event, p, k, bits_mismatch_tolerance, seed_gen, verbose = false)
+    Random.seed!(seed_gen)              # initializing the random bit generator
+    reg = Register(1)                   # represents the quantum channel
+    alice_bases = rand(0:1, L_init)     # Alice's random bases. 0 = Z-basis, 1 = X-basis
+    alice_bits = rand(0:1, L_init)      # Alice's random bits
+    alice_quantum_key = Int[]           # Alice's derived key
+    bob_bases = rand(0:1, L_init)       # Bob's random bases
+    bob_bits = []                       # Bob's measured bits
+    bob_quantum_key = Int[]             # Bob's derived key
+    eves_bases = rand(0:1, L_init)      # Eve's random bases
+    eves_bits = []                      # Eve's measured bits
 
-Random.seed!(seed_gen)              # initializing the random bit generator
-reg = Register(1)                   # represents the quantum channel
-alice_bases = rand(0:1, L_init)     # Alice's random bases. 0 = Z-basis, 1 = X-basis
-alice_bits = rand(0:1, L_init)      # Alice's random bits
-alice_quantum_key = Int[]           # Alice's derived key
-bob_bases = rand(0:1, L_init)       # Bob's random bases
-bob_bits = []                       # Bob's measured bits
-bob_quantum_key = Int[]             # Bob's derived key
-eves_bases = rand(0:1, L_init)      # Eve's random bases
-eves_bits = []                      # Eve's measured bits
-
-for i in 1:L_init
-    # Alice prepares the qubit
-    if alice_bases[i] == 0
-        # Z-basis
-        if alice_bits[i] == 0
-            current_qubit = Z₁  #|0⟩
+    for i in 1:L_init
+        # Alice prepares the qubit
+        if alice_bases[i] == 0
+            # Z-basis
+            if alice_bits[i] == 0
+                current_qubit = Z₁  #|0⟩
+            else
+                current_qubit = Z₂  #|1⟩
+            end
         else
-            current_qubit = Z₂  #|1⟩
+            # X-basis
+            if alice_bits[i] == 0
+                current_qubit = X₁ # |+⟩
+            else
+                current_qubit = X₂ # |−⟩
+            end
         end
-    else
-        # X-basis
-        if alice_bits[i] == 0
-            current_qubit = X₁ # |+⟩
+
+        rho = SProjector(current_qubit)
+        if bit_flip_event && phase_flip_event
+            # both, represented by the operator XZ = Y (I hope)
+            rho_after_noise = (1 - p) * rho + p * Y * rho * Y
+        elseif bit_flip_event
+            # bit flip with probability p
+            rho_after_noise = (1 - p) * rho + p * X * rho * X
+        elseif phase_flip_event
+            # phase flip with probability p
+            rho_after_noise = (1 - p) * rho + p * Z * rho * Z
         else
-            current_qubit = X₂ # |−⟩
+            # no noise, pure state
+            rho_after_noise = rho
         end
-    end
+        initialize!(reg, 1, rho_after_noise)
 
-    rho = SProjector(current_qubit)
-    if bit_flip_event & phase_flip_event
-        # both, represented by the operator XZ = Y (I hope)
-        rho_after_noise = (1 - p) * rho + p * Y * rho * Y
-    elseif bit_flip_event
-        # bit flip with probability p
-        rho_after_noise = (1 - p) * rho + p * X * rho * X
-    elseif phase_flip_event
-        # phase flip with probability p
-        rho_after_noise = (1 - p) * rho + p * Z * rho * Z
-    else
-        # no noise, pure state
-        rho_after_noise = rho
-    end
-    initialize!(reg, 1, rho_after_noise)
+        if eavesdropping_event
+            # simulate eavesdropping event
+            if eves_bases[i] == 0
+                eves_result = project_traceout!(reg, 1, [Z₁, Z₂]) - 1   # -1 to convert from 1/2 to 0/1
+                initialize!(reg[1], eves_result == 0 ? Z₁ : Z₂)         # depending on the outcome the qubit gets initialized
+            else
+                eves_result = project_traceout!(reg, 1, [X₁, X₂]) - 1
+                initialize!(reg[1], eves_result == 0 ? X₁ : X₂)
+            end
+            push!(eves_bits, eves_result)
+        end
 
-    if eavesdropping_event
-        # simulate eavesdropping event
-        if eves_bases[i] == 0
-            eves_result = project_traceout!(reg, 1, [Z₁, Z₂]) - 1   # -1 to convert from 1/2 to 0/1
-            initialize!(reg[1], eves_result == 0 ? Z₁ : Z₂)         # depending on the outcome the qubit gets initialized
+        # Bob measures the qubit
+        if bob_bases[i] == 0
+            # Z-basis measurement
+            meas_result = project_traceout!(reg, 1, [Z₁, Z₂]) - 1
         else
-            eves_result = project_traceout!(reg, 1, [X₁, X₂]) - 1
-            initialize!(reg[1], eves_result == 0 ? X₁ : X₂)
+            # X-basis measurement
+            meas_result = project_traceout!(reg, 1, [X₁, X₂]) - 1
         end
-        push!(eves_bits, eves_result)
+
+        # Store Bob's measurement result
+        push!(bob_bits, meas_result)
     end
 
-    # Bob measures the qubit
-    if bob_bases[i] == 0
-        # Z-basis measurement
-        meas_result = project_traceout!(reg, 1, [Z₁, Z₂]) - 1
-    else
-        # X-basis measurement
-        meas_result = project_traceout!(reg, 1, [X₁, X₂]) - 1
+    # seperation of the quantum key by extracting only the bits associated to matching bases
+    for i in 1:L_init
+        if alice_bases[i] == bob_bases[i]
+            push!(alice_quantum_key, alice_bits[i])
+            push!(bob_quantum_key, bob_bits[i])
+            if verbose && alice_bits[i] != bob_bits[i]
+                # print for logging purposes
+                println("different key bit at index ", i)
+            end
+        end
     end
+    println("Sifted key length: ", length(alice_quantum_key))
 
-    # Store Bob's measurement result
-    push!(bob_bits, meas_result)
-end
+    # disclosure of bits for checking if eavesdropping has occurred
+    quantum_key_length = length(alice_quantum_key)
+    n_disclosed_bits = Int(floor(k * quantum_key_length))
+    mismatched_disclosed_bits = 0
+    for i in 1:n_disclosed_bits
+        if alice_quantum_key[i] != bob_quantum_key[i]
+            mismatched_disclosed_bits += 1
+        end
+    end
+    println("mismatched bits found in the disclosed bits: ", mismatched_disclosed_bits)
 
-# seperation of the quantum key by extracting only the bits associated to matching bases
-for i in 1:L_init
-    if alice_bases[i] == bob_bases[i]
-        push!(alice_quantum_key, alice_bits[i])
-        push!(bob_quantum_key, bob_bits[i])
+
+    # ----- METRICS -----
+    # global mismatch ratio
+    # ratio of the total number of mismatched bits between Alice's and Bob to L_init
+    global_R_miss = 0
+    mismatched_bits = 0
+    for i in 1:L_init
         if alice_bits[i] != bob_bits[i]
-            # print for logging purposes
-            println("different key bit at index ", i)
+            mismatched_bits += 1
         end
     end
-end
-println("Sifted key length: ", length(alice_quantum_key))
+    global_R_miss = mismatched_bits / L_init
+    println("global mismatch ratio: ", global_R_miss)
 
-# disclosure of bits for checking if eavesdropping has occurred
-quantum_key_length = length(alice_quantum_key)
-n_disclosed_bits = Int(floor(k * quantum_key_length))
-mismatched_disclosed_bits = 0
-for i in 1:n_disclosed_bits
-    if alice_quantum_key[i] != bob_quantum_key[i]
-        mismatched_disclosed_bits += 1
-    end
-end
-println("mismatched bits found in the disclosed bits: ", mismatched_disclosed_bits)
+    # Z mismatch ratio, X mismatch ratio
+    # 1) ratio of mismatched bits to L_init when states are encoded in the Z basis {|0⟩, |1⟩}
+    # 2) ratio of mismatched bits to L_init when states are encoded in the X basis {|+⟩, |-⟩}
 
-
-# ----- METRICS -----
-# global mismatch ratio
-# ratio of the total number of mismatched bits between Alice's and Bob to L_init
-global_R_miss = 0
-mismatched_bits = 0
-for i in 1:L_init
-    if alice_bits[i] != bob_bits[i]
-        mismatched_bits += 1
-    end
-end
-global_R_miss = mismatched_bits / L_init
-println("global mismatch ratio: ", global_R_miss)
-
-# Z mismatch ratio, X mismatch ratio
-# 1) ratio of mismatched bits to L_init when states are encoded in the Z basis {|0⟩, |1⟩}
-# 2) ratio of mismatched bits to L_init when states are encoded in the X basis {|+⟩, |-⟩}
-
-n_Z_base = 0
-n_Z_base_mismatch = 0
-n_X_base = 0
-n_X_base_mismatch = 0
-for i in 1:L_init
-    if alice_bases[i] == 0
-        n_Z_base += 1
-        if alice_bits[i] != bob_bits[i]
-            n_Z_base_mismatch += 1
+    n_Z_base = 0
+    n_Z_base_mismatch = 0
+    n_X_base = 0
+    n_X_base_mismatch = 0
+    for i in 1:L_init
+        if alice_bases[i] == 0
+            n_Z_base += 1
+            if alice_bits[i] != bob_bits[i]
+                n_Z_base_mismatch += 1
+            end
+        end
+        if alice_bases[i] == 1
+            n_X_base += 1
+            if alice_bits[i] != bob_bits[i]
+                n_X_base_mismatch += 1
+            end
         end
     end
-    if alice_bases[i] == 1
-        n_X_base += 1
-        if alice_bits[i] != bob_bits[i]
-            n_X_base_mismatch += 1
-        end
-    end
+    Z_R_miss = n_Z_base_mismatch / n_Z_base
+    X_R_miss = n_X_base_mismatch / n_X_base
+    println("Z mismatch ratio: ", Z_R_miss)
+    println("X mismatch ratio: ", X_R_miss)
+
+    P_undetected = 0
+
+    return global_R_miss, Z_R_miss, X_R_miss, P_undetected
 end
-Z_R_miss = n_Z_base_mismatch / n_Z_base
-X_R_miss = n_X_base_mismatch / n_X_base
-println("Z mismatch ratio: ", Z_R_miss)
-println("X mismatch ratio: ", X_R_miss)
-
-
-# eavesdropping detection probability
-#p_und(k) = 1-(3/4)^k
-#ks = 0:0.1:20               # define the range of k
-#fs = p_und.(ks)             # calc function values
-
-# printFigure
-#fig = Figure(size=(800,400))
-#ax = Axis(fig[1, 1], xlabel="k", ylabel="f(k)", title="f(k) = 1 - (3/4)^k")
-
-# Plot zeichnen
-#lines!(ax, ks, fs, color=:blue, linewidth=2)
-
-# Figure anzeigen
-#fig
---
+++ src/BB84_Simulations.jl
@@ -0,0 +1,83 @@
+using QuantumSavory
+using GLMakie
+using Random
+using DataFrames
+using CSV
+
+include("BB84_Protocol.jl")
+
+# ----- DEFINING PARAMETERS RANGES -----
+L_init =                        300     # total exchanged bits
+eavesdropping_event_r =         [false, true]   # if there is eavesdropping in the quantum channel
+bit_flip_event_r =              [false, true]   # if bit flip events happen
+phase_flip_event_r =            [false, true]   # if phase flip events happen
+p_r =                           0.0:0.1:1.0     # probability of error in the quantum channel
+k_r =                           0.0:0.1:1.0     # fraction of the disclosed key for eavesdropping detection
+bits_mismatch_tolerance_r =     0.0:0.1:1.0     # fraction of mismatched key's bits that we are willing to accept before concluding that there ws eavesdropping
+repetition_r =                  1:1:20
+seed =                          0               # initial seed
+
+function mismatch_ratio_experiment(eavesdropping_event, bit_flip_event, phase_flip_event)
+    global seed
+    global p_r
+    global repetition_r
+    df = DataFrame(
+        p = Float32[],
+        repetition = Int[],
+        seed = Int[],
+        global_R_miss = Float32[],
+        Z_R_miss = Float32[],
+        X_R_miss = Float32[],
+    )
+    for (p, repetition) in Iterators.product(p_r, repetition_r)
+        global_R_miss, Z_R_miss, X_R_miss, P_undetected = simulate_bb84(L_init, eavesdropping_event, bit_flip_event, phase_flip_event, p, 0, 0, seed)
+        push!(df, (p = p,
+                    repetition = repetition,
+                    seed = seed,
+                    global_R_miss = global_R_miss,
+                    Z_R_miss = Z_R_miss,
+                    X_R_miss = X_R_miss))
+
+        seed += 1
+    end
+    return df
+end
+
+
+# create folders if not existent
+import Base.Filesystem: mkpath
+mkpath("mismatch_ratio_experiments")
+mkpath("undetected_eavesdropping_experiments")
+
+# ----- MISMATCH RATIO EXPERIMENTS -----
+# ideal channel conditions
+df = mismatch_ratio_experiment(false, false, false)
+CSV.write("mismatch_ratio_experiments/mismatch_ratios_ideal.csv", df; delim=';')
+
+# no eavesdropping, bit flip channel
+df = mismatch_ratio_experiment(false, true, false)
+CSV.write("mismatch_ratio_experiments/mismatch_ratios_bitflip.csv", df; delim=';')
+
+# no eavesdropping, phase flip channel
+df = mismatch_ratio_experiment(false, false, true)
+CSV.write("mismatch_ratio_experiments/mismatch_ratios_phaseflip.csv", df; delim=';')
+
+# no eavesdropping, bit and phase flip channel
+df = mismatch_ratio_experiment(false, true, true)
+CSV.write("mismatch_ratio_experiments/mismatch_ratios_bitphaseflip.csv", df; delim=';')
+
+# eavesdropping, no bit and phase flip channel
+df = mismatch_ratio_experiment(true, false, false)
+CSV.write("mismatch_ratio_experiments/mismatch_ratios_eavesdropping.csv", df; delim=';')
+
+# eavesdropping, bit flip channel
+df = mismatch_ratio_experiment(true, true, false)
+CSV.write("mismatch_ratio_experiments/mismatch_ratios_eavesdropping_bitflip.csv", df; delim=';')
+
+# eavesdropping, phase flip channel
+df = mismatch_ratio_experiment(true, true, false)
+CSV.write("mismatch_ratio_experiments/mismatch_ratios_eavesdropping_phaseflip.csv", df; delim=';')
+
+# eavesdropping, bit and phase flip channel
+df = mismatch_ratio_experiment(true, true, true)
+CSV.write("mismatch_ratio_experiments/mismatch_ratios_eavesdropping_bitphaseflip.csv", df; delim=';')
